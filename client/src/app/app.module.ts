f81e873a8853d937de638cbc6d3a0d3033e1bdc1
Hassan Raza <razahassan616@gmail.com>
2022-08-10 01:24:00 +0500

    End of Section 6

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,15 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
-import { FormsModule } from '@angular/forms';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
-import { ToastrModule } from 'ngx-toastr';
+import { FormsModule } from '@angular/forms';
+import { SharedModule } from './_modules/shared.module';
 
 @NgModule({
   declarations: [
@@ -31,9 +30,8 @@ import { ToastrModule } from 'ngx-toastr';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    BsDropdownModule.forRoot(),
     FormsModule,
-    ToastrModule.forRoot({ positionClass: 'toast-bottom-right' }),
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent],
